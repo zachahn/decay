55b7cd2c8207c285bf5c364f35421cd9f61164a0
Zach Ahn <engineering@zachahn.com>
2017-05-31 23:59:04 -0400

    Add two feature tests

--- test/decay_test.rb
+++ test/decay_test.rb
@@ -1,11 +1,48 @@
 require "test_helper"
 
 class DecayTest < TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::Decay::VERSION
+  def test_standalone_usage
+    et = Decay::EnumeratedType.new
+    et[:phone] = "phone"
+    et[:home] = "home"
+
+    assert_raises(Decay::Error) do
+      et.case
+        .when(:phone) {}
+        .result
+    end
+
+    et.case
+      .when(:phone) {}
+      .when(:home) {}
+      .result
+
+    et.case
+      .when(:phone) {}
+      .else {}
+      .result
   end
 
-  def test_it_does_something_useful
-    assert false
+  class Meta
+    include Decay::Enum
+
+    enum model: %i[zoolander hansel]
+  end
+
+  def test_meta_usage
+    meta = Meta.new
+
+    meta.model = :zoolander
+
+    assert_raises(Decay::Error) do
+      meta.model.case
+        .when(:zoolander) {}
+        .result
+    end
+
+    meta.model.case
+      .when(:zoolander) {}
+      .when(:hansel) {}
+      .result
   end
 end
